dab933454515b2b3d16a3c2cfb16ddc46934a9ae
ʕ·ᴥ·ʔAKJ <arcage@denchu.org>
2022-12-08 17:43:47 +0900

    Replace duplicated calls to variables.

--- day07/day07_part2.cr
+++ day07/day07_part2.cr
@@ -2,18 +2,20 @@ alias S=String
 record D,n = "",c={}of S=>D|F{def s : Int32;c.map{|n,i|i.s}.sum;end}
 record F,n : S,s : Int32
 T=[D.new()]
-t=[T[0]]
+r=T[0]
+t=[r]
 STDIN.gets_to_end.split(/\n?\$ /).map(&.split('\n')).each{|o|m=o.shift.split(' ')
+d=t[-1]
 if m[0]=="cd"
-case m[1]
+case h=m[1]
 when ".."
 t.pop
 when "/"
-t=[t[0]]
+t=[r]
 else
-t<<t[-1].c[m[1]].as(D)
+t<<d.c[h].as(D)
 end
 else
-o.each{|e|l,n=e.split(' ');t[-1].c[n]=(l=="dir"? (T<<D.new(n))[-1]: F.new(n,l.to_i))}
+o.each{|e|l,n=e.split(' ');d.c[n]=(l=="dir"? (T<<D.new(n))[-1]: F.new(n,l.to_i))}
 end}
-p T.map(&.s).select(&.>=(T[0].s-4e7.to_i)).min
+p T.map(&.s).select(&.>=(r.s-4e7.to_i)).min
